7fbc6081bd65a99ac17ef0ad003d4c7a71e214c7
sezm00 <islam.abdelmotaleb23d@eslsca.edu.eg>
2025-02-27 16:33:45 +0200

    text postioning

--- client/client/src/components/MainContent.jsx
+++ client/client/src/components/MainContent.jsx
@@ -15,27 +15,52 @@ function MainContent() {
       </div>
       <XSection 
         title="XConstruction" 
-        text="Lorem ipsum odor amet, consectetuer adipiscing elit. Neque etiam leo egestas finibus mollis pharetra." 
+        text={
+          <>
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            <br />
+            Neque etiam leo egestas finibus mollis pharetra.
+          </>
+        } 
         imgSrc="./img/Asset 82@300x.png" 
         reverse={true} 
       />
+
       
       <XSection 
         title="XAcademy" 
-        text="Lorem ipsum odor amet, consectetuer adipiscing elit. Neque etiam leo egestas finibus mollis pharetra." 
+        text={
+          <>
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            <br />
+            Neque etiam leo egestas finibus mollis pharetra.
+          </>
+        } 
         imgSrc="./img/Asset 79@300x.png" 
       />
       
       <XSection 
         title="XFinance" 
-        text="Lorem ipsum odor amet, consectetuer adipiscing elit. Neque etiam leo egestas finibus mollis pharetra." 
+        text={
+          <>
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            <br />
+            Neque etiam leo egestas finibus mollis pharetra.
+          </>
+        } 
         imgSrc="./img/Asset 80@300x.png" 
         reverse={true} 
       />
       
       <XSection 
         title="XStudios" 
-        text="Lorem ipsum odor amet, consectetuer adipiscing elit. Neque etiam leo egestas finibus mollis pharetra." 
+        text={
+          <>
+            Lorem ipsum odor amet, consectetuer adipiscing elit.
+            <br />
+            Neque etiam leo egestas finibus mollis pharetra.
+          </>
+        } 
         imgSrc="./img/Asset 81@300x.png" 
       />
     </div>
