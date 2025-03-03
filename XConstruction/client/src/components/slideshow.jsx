7cea967bcc0fe74f09de91d573091d022edfb1ef
sezm00 <islam.abdelmotaleb23d@eslsca.edu.eg>
2025-03-03 23:50:22 +0200

    Create Slide Show Component JSX & CSS files

---
+++ XConstruction/client/src/components/slideshow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+import "../style/slideshow.css";
+
+function SildeShow(){
+    return(
+        <div className="main-container-slide">         
+        </div>
+    );
+}
+
+export default SildeShow;
