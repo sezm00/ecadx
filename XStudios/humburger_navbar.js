5166473b712558ff1058eb228f7cd4475b6d7f8a
sezm00 <islam.abdelmotaleb02@gmail.com>
2025-09-26 01:21:18 +0300

    XStudios index.html test

---
+++ XStudios/humburger_navbar.js
@@ -0,0 +1,6 @@
+    const menuToggle = document.getElementById("menu-toggle");
+    const navbar = document.querySelector(".navbar");
+
+    menuToggle.addEventListener("click", () => {
+      navbar.classList.toggle("active");
+    });
